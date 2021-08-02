d26e0b8d7b8b466f10ee86145b9393d51f1c5aa3
tomytjandra <tomytjandra@gmail.com>
2021-08-02 16:26:06 +0700

    modularized version

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# MNIST-digit-classifier-app
+# MNIST Handwritten Digits Classifier
+
+Deployed app: https://share.streamlit.io/tomytjandra/mnist-digit-classifier-app
+
+## Project Structure
+
+- Folders:
+    - `draft`: Python Notebook for testing
+
+- Files:
+    - `model.pt`: Pre-trained model using Pytorch (ANN)
+    - `requirements.txt`: Package dependencies
+    - `model.py`: Load model and predict function
+    - `streamlit_app.py`: Main `streamlit` application
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,54 +1,14 @@
-# image processing
-from PIL import Image
-import numpy as np
-
 # visualization
-import matplotlib.image as mimage
 import matplotlib.pyplot as plt
 plt.style.use('seaborn')
 
 # modelling
 import torch
-import torch.nn as nn
-import torchvision.transforms as T
+from model import predict_proba
 
 # web app
 import streamlit as st
 from streamlit_drawable_canvas import st_canvas
-
-# LOAD PRETRAINED MODEL
-input_size = 784
-hidden_sizes = [128, 64]
-output_size = 10
-model = nn.Sequential(
-    nn.Linear(input_size, hidden_sizes[0]),
-    nn.ReLU(),
-    nn.Linear(hidden_sizes[0], hidden_sizes[1]),
-    nn.ReLU(),
-    nn.Linear(hidden_sizes[1], output_size),
-    nn.LogSoftmax(dim=1)
-    )
-model.load_state_dict(torch.load('model.pt'))
-
-# PREDICT FUNCTION
-def predict_proba(image):
-    # process image
-    image = Image.fromarray(image.astype(np.uint8)).convert('L')
-    image = image.resize((28, 28))
-    
-    # convert to tensor
-    transforms = T.ToTensor()
-    tensor = transforms(image)
-
-    # if canvas is not drawn
-    if tensor.min().item() == tensor.max().item():
-        return image, None
-
-    # predict
-    with torch.no_grad():
-        output = model(tensor.view(1,-1))
-        prob = torch.exp(output)
-    return image, prob.view(-1)
 
 # PAGE CONFIGURATION
 st.set_page_config(
